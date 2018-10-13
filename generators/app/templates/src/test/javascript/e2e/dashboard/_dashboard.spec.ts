c3ef731f0626ad9d472d30aa96729d4c9e8ecddb
Pascal Grimaud <pascalgrimaud@gmail.com>
2018-10-13 21:27:47 +0200

    Fix Protractor tests

--- generators/app/templates/src/test/javascript/e2e/dashboard/_dashboard.spec.ts
+++ generators/app/templates/src/test/javascript/e2e/dashboard/_dashboard.spec.ts
@@ -1,5 +1,8 @@
 import { browser, element, by } from 'protractor';
-import { NavBarPage } from './../page-objects/jhi-page-objects';
+import { NavBarPage, SignInPage } from './../page-objects/jhi-page-objects';
+
+const expect = chai.expect;
+
 <%_
 let elementGetter = `getText()`;
 if (enableTranslation) {
@@ -7,95 +10,95 @@ if (enableTranslation) {
 }
 _%>
 
-describe('administration', () => {
+describe('dashboard', () => {
     let navBarPage: NavBarPage;
+    let signInPage: SignInPage;
     const dashboardMenu = element(by.id('dashboard-menu'));
 
-    beforeAll(() => {
-        browser.get('/');
-        browser.waitForAngular();
+    before(async () => {
+        await browser.get('/');
         navBarPage = new NavBarPage(true);
-        navBarPage.getSignInPage().autoSignInUsing('admin', 'admin');
-        browser.waitForAngular();
+        signInPage = await navBarPage.getSignInPage();
+        await signInPage.autoSignInUsing('admin', 'admin');
     });
 
-    beforeEach(() => {
-        dashboardMenu.click();
+    beforeEach(async () => {
+        await dashboardMenu.click();
     });
 
-    it('should load BarChart', () => {
-        element(by.css('[routerLink="barchart"]')).click();
+    it('should load BarChart', async () => {
+        await element(by.css('[routerLink="barchart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.barchart.home.title/;
         <%_ } else { _%>
         const expect1 = /BarChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    it('should load DoughnutChart', () => {
-        element(by.css('[routerLink="doughnutchart"]')).click();
+    it('should load DoughnutChart', async () => {
+        await element(by.css('[routerLink="doughnutchart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.doughnutchart.home.title/;
         <%_ } else { _%>
         const expect1 = /DoughnutChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    it('should load LineChart', () => {
-        element(by.css('[routerLink="linechart"]')).click();
+    it('should load LineChart', async () => {
+        await element(by.css('[routerLink="linechart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.linechart.home.title/;
         <%_ } else { _%>
         const expect1 = /LineChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    it('should load PieChart', () => {
-        element(by.css('[routerLink="piechart"]')).click();
+    it('should load PieChart', async () => {
+        await element(by.css('[routerLink="piechart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.piechart.home.title/;
         <%_ } else { _%>
         const expect1 = /PieChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    it('should load PolarAreaChart', () => {
-        element(by.css('[routerLink="polarareachart"]')).click();
+    it('should load PolarAreaChart', async () => {
+        await element(by.css('[routerLink="polarareachart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.polarareachart.home.title/;
         <%_ } else { _%>
         const expect1 = /PolarAreaChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    it('should load RadarChart', () => {
-        element(by.css('[routerLink="radarchart"]')).click();
+    it('should load RadarChart', async () => {
+        await element(by.css('[routerLink="radarchart"]')).click();
         <%_ if (enableTranslation) { _%>
         const expect1 = /dashboard.radarchart.home.title/;
         <%_ } else { _%>
         const expect1 = /RadarChart/;
         <%_ } _%>
         element.all(by.css('h2 span')).first().<%- elementGetter %>.then(value => {
-            expect(value).toMatch(expect1);
+            expect(value).to.eq(expect1);
         });
     });
 
-    afterAll(() => {
-        navBarPage.autoSignOut();
+    after(async () => {
+        await navBarPage.autoSignOut();
     });
 });
